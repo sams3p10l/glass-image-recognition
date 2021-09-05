6c89d8ce3de05b78742065e9b9745f5a06794b3d
Mihailo <mihailomarkovic096@gmail.com>
2021-09-05 23:05:54 +0200

    Implemented Glass gesture detector

---
+++ app/src/main/java/com/sams3p10l/diplomski/ui/BaseActivity.kt
@@ -0,0 +1,58 @@
+package com.sams3p10l.diplomski.ui
+
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.sams3p10l.diplomski.gesture.GlassGestureDetector
+
+abstract class BaseActivity : AppCompatActivity(), GlassGestureDetector.OnGestureListener {
+
+    private lateinit var decorView: View
+    private lateinit var glassGestureDetector: GlassGestureDetector
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+
+        decorView = window.decorView
+        decorView.setOnSystemUiVisibilityChangeListener { visibility ->
+            if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
+                hideSystemUI()
+            }
+        }
+
+        glassGestureDetector = GlassGestureDetector(this, this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUI()
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (glassGestureDetector.onTouchEvent(ev)) {
+            return true
+        }
+        return super.dispatchTouchEvent(ev)
+    }
+
+    override fun onGesture(gesture: GlassGestureDetector.Gesture?): Boolean {
+        return when (gesture) {
+            GlassGestureDetector.Gesture.SWIPE_DOWN -> {
+                finish()
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun hideSystemUI() {
+        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+}
--- app/src/main/java/com/sams3p10l/diplomski/ui/MainActivity.kt
+++ app/src/main/java/com/sams3p10l/diplomski/ui/MainActivity.kt
@@ -3,9 +3,13 @@ package com.sams3p10l.diplomski.ui
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.sams3p10l.diplomski.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     companion object {
         val TAG: String = MainActivity::class.java.simpleName
     }
@@ -15,5 +19,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.d(TAG, "onCreate: IT'S ALIVE")
+    }
+
+    private inner class ScreenSlidePagerAdapter(fm: FragmentManager) :
+        FragmentStateAdapter(fm, lifecycle) {
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            TODO("Not yet implemented")
+        }
+
     }
 }
