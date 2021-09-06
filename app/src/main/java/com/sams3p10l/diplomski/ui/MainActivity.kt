ac18200413e78b6abd959aaabb564cb542cf8064
Mihailo <mihailomarkovic096@gmail.com>
2021-09-07 01:20:16 +0200

    Fragment initialization; populating viewpager

--- app/src/main/java/com/sams3p10l/diplomski/ui/MainActivity.kt
+++ app/src/main/java/com/sams3p10l/diplomski/ui/MainActivity.kt
@@ -1,36 +1,54 @@
 package com.sams3p10l.diplomski.ui
 
 import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.sams3p10l.diplomski.R
+import com.sams3p10l.diplomski.databinding.ActivityMainBinding
+import com.sams3p10l.diplomski.ui.fragment.ActionFragment
+import com.sams3p10l.diplomski.ui.fragment.HelpFragment
+import com.sams3p10l.diplomski.ui.fragment.SettingsFragment
+import com.sams3p10l.diplomski.util.FragmentFactoryUtil
 
 class MainActivity : BaseActivity() {
     companion object {
         val TAG: String = MainActivity::class.java.simpleName
     }
 
+    private lateinit var binding: ActivityMainBinding
+    private val fragments = arrayListOf<Fragment>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        Log.d(TAG, "onCreate: IT'S ALIVE")
+        val screenSlidePagerAdapter = ScreenSlidePagerAdapter(supportFragmentManager)
+        binding.viewpager.adapter = screenSlidePagerAdapter
+
+        populateFragmentList()
+        screenSlidePagerAdapter.notifyDataSetChanged()
+
+        binding.tabLayout.setupWithViewPager(binding.viewpager, true)
     }
 
-    private inner class ScreenSlidePagerAdapter(fm: FragmentManager) :
-        FragmentStateAdapter(fm, lifecycle) {
+    private fun populateFragmentList() {
+        fragments.apply {
+            add(FragmentFactoryUtil.create(ActionFragment.TAG, "Action", "Test"))
+            add(FragmentFactoryUtil.create(SettingsFragment.TAG, "Settings", "Test"))
+            add(FragmentFactoryUtil.create(HelpFragment.TAG, "Action", "Test"))
+        }
+    }
 
-        override fun getItemCount(): Int {
-            TODO("Not yet implemented")
+    inner class ScreenSlidePagerAdapter(fm: FragmentManager) :
+        FragmentStatePagerAdapter(fm) {
+
+        override fun getCount(): Int {
+            return fragments.size
         }
 
-        override fun createFragment(position: Int): Fragment {
-            TODO("Not yet implemented")
+        override fun getItem(position: Int): Fragment {
+            return fragments[position]
         }
-
     }
 }
