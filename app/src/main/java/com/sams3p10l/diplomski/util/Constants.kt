75fdf6f243e9d6d6d9f888eb88c361ad6ae37669
Mihailo <mihailomarkovic096@gmail.com>
2021-09-07 01:20:04 +0200

    Added FragmentFactoryUtil for creating fragments

---
+++ app/src/main/java/com/sams3p10l/diplomski/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.sams3p10l.diplomski.util
+
+object Constants {
+    const val TEXT_KEY = "text_key"
+    const val FOOTER_KEY = "footer_key"
+    const val TIMESTAMP_KEY = "timestamp_key"
+    const val BODY_TEXT_SIZE: Int = 40
+}
