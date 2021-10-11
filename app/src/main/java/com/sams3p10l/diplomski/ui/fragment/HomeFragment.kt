cc1cc5aedd88e6341b2879ba3d367481b58de327
Mihailo <mihailomarkovic096@gmail.com>
2021-10-11 02:12:39 +0200

    Navigating to ActionFragment

--- app/src/main/java/com/sams3p10l/diplomski/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/sams3p10l/diplomski/ui/fragment/HomeFragment.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.ExperimentalGetImage
 import androidx.fragment.app.Fragment
 import com.sams3p10l.diplomski.databinding.FragmentHomeBinding
 import com.sams3p10l.diplomski.util.Constants.FOOTER_KEY
 import com.sams3p10l.diplomski.util.Constants.TEXT_KEY
 
+
+@ExperimentalGetImage
 class HomeFragment : Fragment() {
     companion object {
         val TAG: String = HomeFragment::class.java.name
     }
 
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var function: String
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,9 +27,20 @@ class HomeFragment : Fragment() {
     ): View {
         binding = FragmentHomeBinding.inflate(inflater)
 
-        binding.hfTitle.text = arguments?.getString(TEXT_KEY).orEmpty()
+        function = arguments?.getString(TEXT_KEY).orEmpty().also { binding.hfTitle.text = it }
         binding.hfFooter.text = arguments?.getString(FOOTER_KEY).orEmpty()
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.root.setOnClickListener {
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.replace(it.id, ActionFragment(), ActionFragment.TAG)
+                ?.addToBackStack(null)
+                ?.commit()
+        }
     }
 }
